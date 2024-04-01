783c5f3f73ada050dfb3708e648ab5cef072e0b0
WildanYulianto <162247029+WildanYulianto@users.noreply.github.com>
2024-04-01 12:16:39 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+var lastOperation = "";
+
+function setLastOperation(operation) {
+    lastOperation = operation;
+}
+
+function display(value) {
+    var result = document.getElementById("result");
+    result.value += value;
+}
+
+function ClearScreen() {
+    var result = document.getElementById("result");
+    result.value = "";
+    lastOperation ="";
+}
+
+function calculate() {
+    var result = document.getElementById("result");
+    var p = result.value.trim();
+    if (p !== "") {
+        var q = eval(p);
+        if (!isNaN(q)) {
+            result.value = q;
+        } else {
+            alert("Invalid input.");
+        }
+    } else {
+        alert("please enter a valid expression.");
+    }
+}
+
+function deleteLastCharacter() {
+    var result = document.getElementById('result');
+    result.value = result.value.slice(0, -1);
+}
+
+function trig(func) {
+    setLastOperation(func);
+    var result = document.getElementById('result');
+    result.value = Math[func](parseFloat(result.value));
+}
+
+function power() {
+    setLastOperation("^");
+    var result = document.getElementById('result');
+    result.value = Math.pow(parseFloat(result.value), 2);
+}
+
+function sqrt() {
+    setLastOperation("sqrt");
+    var result = document.getElementById('result');
+    result.value = Math.sqrt(parseFloat(result.value));
+}
+
+function log() {
+    setLastOperation("log");
+    var result = document.getElementById('result');
+    result.value = Math.log10(parseFloat(result.value));
+}
+
+function ln() {
+    setLastOperation("ln");
+    var result = document.getElementById('result');
+    result.value = Math.log(parseFloat(result.value));
+}
